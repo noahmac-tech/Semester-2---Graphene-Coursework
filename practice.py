20b6f77d5a7a4a429330d518de14154ae4598a4c
noahmac-tech <noahmccahill123@gmail.com>
2026-02-03 14:36:04 +0000

    Defined Nearest neighbour sites and calculated structure factor

--- practice.py
+++ practice.py
@@ -15,6 +15,20 @@ ky = np.linspace(-K,K,N)
 
 KX, KY = np.meshgrid(kx, ky)
 
+"""Define Nearest Neighbour sites"""
+"""Choosing 0 as origin"""
+
+sites = np.array([
+    [0, a/(np.sqrt(3))], # Up
+    [a/2, -a/(2*np.sqrt(3))], #Down Right
+    [-a/2, -a/(2*np.sqrt(3))]]) #Down Left
+
+def f(kx, ky, sites):
+    sum = np.zeros_like(KX, dtype=complex)
+    for site in sites:
+        rx, ry = site
+        sum += np.exp(1j * (kx * rx + ky * ry))
+    return sum
 
 """Calculate positive and negative surfaces"""
 """Plot on same 3D axes"""
