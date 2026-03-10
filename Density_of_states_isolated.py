8a1826612e0e045a3854ce32f8550ef5cedefdeb
noahmac-tech <noahmccahill123@gmail.com>
2026-03-10 15:25:23 +0000

    imported argparse

--- Density_of_states_isolated.py
+++ Density_of_states_isolated.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.sparse.linalg import eigsh
 
+import argparse
+
 from twisted_bilayer_graphene import (
     reciprocal_vectors,
     build_sparse_H_tbg_k,
